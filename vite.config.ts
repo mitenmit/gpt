de574b3183cfe474227ff497eeefbc48d96f0013
am1006 <am1006@users.noreply.github.com>
2024-04-02 18:35:55 +1100

    Vite: remove remaining tests and move config to mts

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "/",
-  plugins: [react()],
-  publicDir: "public",
-});
